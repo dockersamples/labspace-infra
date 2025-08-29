8cc26ca520ce8930782830b40dd83335f03e3edf
Michael Irwin <mikesir87@gmail.com>
2025-08-28 22:47:29 -0400

    Add dark mode support for the interface
    Resolves #38

--- components/interface/client/src/main.jsx
+++ components/interface/client/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", "dark");
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
