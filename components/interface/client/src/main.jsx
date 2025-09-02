66670f4e65426b47eadbe10903f01eafb4a9319a
Michael Irwin <mikesir87@gmail.com>
2025-09-02 17:01:12 -0400

    Watch for dark mode changes in the interface
    Resolves #56

--- components/interface/client/src/main.jsx
+++ components/interface/client/src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", "dark");
+const darkModeMatcher = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (darkModeMatcher.matches) {
+  document
+    .getElementsByTagName("html")[0]
+    .setAttribute("data-bs-theme", "dark");
 }
+
+darkModeMatcher.addEventListener("change", (event) => {
+  if (event.matches) {
+    document
+      .getElementsByTagName("html")[0]
+      .setAttribute("data-bs-theme", "dark");
+  } else {
+    document
+      .getElementsByTagName("html")[0]
+      .setAttribute("data-bs-theme", "light");
+  }
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
