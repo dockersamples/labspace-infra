9a2b49bec51d62d55722463c0c7e4e1669721b54
Michael Irwin <mikesir87@gmail.com>
2025-09-09 13:11:38 -0400

    Revert "Move host port republishing from separate service to support extension"
    This reverts commit 83fbc118d203aebd3e2236014a5c8664960845ef.

--- components/support-vscode-extension/src/extension.js
+++ components/support-vscode-extension/src/extension.js
@@ -1,73 +1,45 @@
 const vscode = require('vscode');
 const { ExpressSocketServer } = require("./socketServer.js");
-const { HostPortRepublisher } = require("./hostPortRepublisher.js");
 
-let socketServer;
-let hostPortRepublisher;
-let logChannel;
+let server;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    logChannel = vscode.window.createOutputChannel("Labspace Support");
+	const start = () => {
+        stop();
+        server = new ExpressSocketServer(
+			vscode.workspace.getConfiguration('labRunner'), 
+			context.asAbsolutePath('package.json')
+		);
+        server.start();
+    };
 
-    startSocketServer(context, logChannel);
-    startHostPortRepublisher(logChannel);
+    const stop = () => {
+        if (server) {
+            server.dispose();
+            server = undefined;
+        }
+    };
+
+    start();
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('labspace-support.restartCommandServer', () => {
-            startSocketServer(context, logChannel);
-            vscode.window.showInformationMessage('Labspace Support: restart complete');
+        vscode.commands.registerCommand('labRunner.restartServer', () => {
+            vscode.window.showInformationMessage('Lab Runner: restarting HTTP socket server...');
+            start();
         }),
-        { dispose: stopSocketServer }
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand('labspace-support.restartPortRepublisher', () => {
-            startHostPortRepublisher(logChannel);
-            vscode.window.showInformationMessage('Labspace Support: restart complete');
-        }),
-        { dispose: stopPortRepublisher }
+        { dispose: stop }
     );
 }
 
-function startSocketServer(context, logChannel) {
-    stopSocketServer();
-    socketServer = new ExpressSocketServer(
-        vscode.workspace.getConfiguration('labspace-support'), 
-        context.asAbsolutePath('package.json'),
-        logChannel
-    );
-    socketServer.start();
-}
-
-function stopSocketServer() {
-    if (socketServer) {
-        socketServer.dispose();
-        socketServer = undefined;
-    }
-}
-
-function startHostPortRepublisher(logChannel) {
-    hostPortRepublisher = new HostPortRepublisher(
-        process.env.LABEL_FILTER ? process.env.LABEL_FILTER.split(',') : ["demo=app"],
-        logChannel
-    );
-    hostPortRepublisher.start();
-}
-
-function stopPortRepublisher() {
-    if (hostPortRepublisher) {
-        hostPortRepublisher.close();
-        hostPortRepublisher = undefined;
-    }
-}
-
-// This method is called when the extension is deactivated
+// This method is called when your extension is deactivated
 function deactivate() {
-	stopSocketServer();
-    stopPortRepublisher();
+	if (server) {
+        server.dispose();
+        server = undefined;
+    }
 }
 
 module.exports = {
