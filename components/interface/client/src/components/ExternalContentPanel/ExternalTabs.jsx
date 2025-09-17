0ffdcf5c69fafc1532d3cc0cac6388048f16902f
Michael Irwin <mikesir87@gmail.com>
2025-09-17 15:31:57 -0400

    Add refresh button to active tab

--- components/interface/client/src/components/ExternalContentPanel/ExternalTabs.jsx
+++ components/interface/client/src/components/ExternalContentPanel/ExternalTabs.jsx
@@ -27,6 +27,21 @@ export function ExternalTabs({
           >
             <span className="me-3">{tab.title}</span>
 
+            {activeTab === tab.url && (
+              <Button
+                size="sm"
+                variant="default"
+                className="rounded-circle p-1 pt-0 pb-0"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  onRefreshClick();
+                }}
+              >
+                <span className="material-symbols-outlined">refresh</span>
+              </Button>
+            )}
+
             {!tab.title !== "Workspace" && (
               <Button
                 size="sm"
