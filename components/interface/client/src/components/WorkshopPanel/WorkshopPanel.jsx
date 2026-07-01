c0674381b37a16a731d87aa0116898ae5510aedf
Michael Irwin <mikesir87@gmail.com>
2026-07-01 09:29:56 -0400

    Force new MarkdownRenderer when changing sections
    This replaces the previous iteration of trying to fix reused
    custom components by forcing a new markdown component for each
    new section. With this, no custom components will be reused as the
    entire tree is swapped on section changes... which feels like the
    right approach anyways

--- components/interface/client/src/components/WorkshopPanel/WorkshopPanel.jsx
+++ components/interface/client/src/components/WorkshopPanel/WorkshopPanel.jsx
@@ -1,12 +1,10 @@
 import { useActiveSection, useWorkshop } from "../../WorkshopContext";
-import { useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import { MarkdownRenderer } from "./markdown/MarkdownRenderer";
 import { WorkshopNav } from "./WorkshopNav";
 import "./WorkshopPanel.scss";
 
 export function WorkshopPanel() {
-  const bodyRef = useRef();
   const { activeSection, changeActiveSection } = useActiveSection();
   const { sections } = useWorkshop();
 
@@ -14,22 +12,16 @@ export function WorkshopPanel() {
     (section) => section.id === activeSection.id,
   );
 
-  useEffect(() => {
-    window.bodyRef = bodyRef.current;
-    setTimeout(
-      () => bodyRef.current.scrollTo({ top: 0, left: 0, behavior: "smooth" }),
-      100,
-    );
-  }, [activeSection?.id, bodyRef]);
-
   const hasNext = index < sections.length - 1;
   const hasPrev = index > 0;
 
   return (
     <div className="d-flex flex-column h-100">
-      <div className="overflow-auto flex-grow-1" ref={bodyRef}>
+      <div className="overflow-auto flex-grow-1">
         <div className="workshop-body p-5 pt-3 pb-3">
-          <MarkdownRenderer>{activeSection.content}</MarkdownRenderer>
+          <MarkdownRenderer key={`section-${activeSection.id}`}>
+            {activeSection.content}
+          </MarkdownRenderer>
         </div>
       </div>
 
